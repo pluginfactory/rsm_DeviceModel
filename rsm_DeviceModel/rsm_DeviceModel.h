3fdda637d4086a670064ce3e0c08598744ebac2d
Rohit Saini <sketchlearn7@gmail.com>
2019-02-16 23:41:55 +0530

    Initial Commit

---
+++ rsm_DeviceModel/rsm_DeviceModel.h
@@ -0,0 +1,19 @@
+//
+//  rsm_DeviceModel.h
+//  rsm_DeviceModel
+//
+//  Created by Rohit Saini on 16/02/19.
+//  Copyright © 2019 Rohit Saini. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for rsm_DeviceModel.
+FOUNDATION_EXPORT double rsm_DeviceModelVersionNumber;
+
+//! Project version string for rsm_DeviceModel.
+FOUNDATION_EXPORT const unsigned char rsm_DeviceModelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <rsm_DeviceModel/PublicHeader.h>
+
+
